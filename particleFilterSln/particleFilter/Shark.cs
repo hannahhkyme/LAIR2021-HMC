1cca7aea843c37628042244749a99fd243d4d300
“hannahhkyme” <hkyme@hmc.edu>
2021-06-23 15:47:14 -0700

    * Shark.cs: * robot.cs:
    * particleFilter.dll: new files

---
+++ particleFilterSln/particleFilter/Shark.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace particleFilter
+{
+    public class Shark
+    {
+        Random random_num = new Random();
+        // SETS TYPE OF MEMBER VARIABLE
+        public double X_S;
+        public double Y_S;
+        public double Z_S;
+        public double THETA_S;
+        public double V_S;
+        public double Current_Time;
+        public Shark()
+        {
+            X_S = 1.0;
+            Y_S = 2.0;
+            Z_S = 3.0;
+            THETA_S = Math.PI;
+            V_S = 3.0;
+            Current_Time = 0.1;
+        }
+
+
+        void update_shark_position()
+        {
+
+        }
+    }
+}
---
+++ particleFilterSln/particleFilter/robot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace particleFilter
+{
+    public class Robot
+    {
+        Random random_num = new Random();
+        // SETS TYPE OF MEMBER VARIABLE
+        public double X_R;
+        public double Y_R;
+        public double Z_R;
+        public double THETA_R;
+        public double V_R;
+        public double Current_Time;
+        public Robot()
+        {
+            X_R = 1.0;
+            Y_R = 2.0;
+            Z_R = 3.0;
+            THETA_R = Math.PI;
+            V_R = 3.0;
+            Current_Time = 0.1;
+        }
+
+
+        void update_robot_position(double velocity, double ang_velocity )
+        {
+
+        }
+
+
+
+        void calc_new_robot_position()
+        {
+
+        }
+
+    }
+}
