c838b2c2bed4e9628bf11a792f080ada8739f609
Roman Herrera <60724440+RomanHerrera1@users.noreply.github.com>
2021-07-01 09:01:07 -0700

    Combined Particle Filter + Plots

---
+++ ParticleFilterVisualization/ParticleFilterVisualization/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ParticleFilterVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.error = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.map = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.error)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // error
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.error.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.error.Legends.Add(legend1);
+            this.error.Location = new System.Drawing.Point(986, 0);
+            this.error.Name = "error";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.error.Series.Add(series1);
+            this.error.Size = new System.Drawing.Size(954, 763);
+            this.error.TabIndex = 0;
+            this.error.Text = "chart1";
+            this.error.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // map
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.map.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.map.Legends.Add(legend2);
+            this.map.Location = new System.Drawing.Point(3, 0);
+            this.map.Name = "map";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.Name = "Series2";
+            this.map.Series.Add(series2);
+            this.map.Series.Add(series3);
+            this.map.Size = new System.Drawing.Size(977, 763);
+            this.map.TabIndex = 1;
+            this.map.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1952, 763);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.error);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.error)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart error;
+        private System.Windows.Forms.DataVisualization.Charting.Chart map;
+    }
+}
+
---
+++ ParticleFilterVisualization/ParticleFilterVisualization/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace ParticleFilterVisualization
+{
+    public partial class Form1 : Form
+    {
+        private Thread cpuThread;
+        private double[] w1xArray = new double[5];
+        private double[] w1yArray = new double[5];
+        private double[] w2xArray = new double[60];
+        private double[] w2yArray = new double[60];
+        private double[] w3xArray = new double[60];
+        private double[] w3yArray = new double[60];
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void getParticleCoordinates()
+        {
+            Random random_num = new Random();
+
+            while (true)
+            {
+                for (int i = 0; i < w1xArray.Length - 1; ++i)
+                {
+                    w1xArray[i] = random_num.Next(-150, 150);
+                    w1yArray[i] = random_num.Next(-150, 150);
+
+
+                    w2xArray[i] = random_num.Next(-150, 150);
+                    w2yArray[i] = random_num.Next(-150, 150);
+                }
+
+                Array.Copy(w1yArray, 0, w1yArray, 0, w1yArray.Length - 1);
+                Array.Copy(w1xArray, 0, w1xArray, 0, w1xArray.Length - 1);
+                if (cpuChart.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateCpuChart(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                if (chart1.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateChart1(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void UpdateCpuChart()
+        {
+            cpuChart.Series["Series1"].Points.Clear();
+            cpuChart.Series["Series2"].Points.Clear();
+            for (int i = 0; i < w1xArray.Length - 1; ++i)
+            {
+                cpuChart.Series["Series1"].Points.AddXY(w1xArray[i], w1yArray[i]);
+                cpuChart.Series["Series2"].Points.AddXY(w2xArray[i], w2yArray[i]);
+            }
+        }
+
+        private void UpdateChart1()
+        {
+            chart1.Series["Series1"].Points.Clear();
+            for (int i = 0; i < w1xArray.Length - 1; ++i)
+            {
+                chart1.Series["Series1"].Points.AddXY(w1xArray[i], w1yArray[i]);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
